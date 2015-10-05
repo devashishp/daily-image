202251015a129b7782384441966d11aeb6a3d754
devashishp <devashish@outlook.com>
2015-10-05 21:48:28 +0530

    Fixes
    Fixed NASA image, Made alternate

--- js/get-image.js
+++ js/get-image.js
@@ -4,7 +4,7 @@ var par = [];
 var Copy = [];
 var inii = 0;
 var swi = 0;
-var incimage = 2;
+var incimage = 1;
 var Dcomp = [];
 var Ncomp = [];
 var Month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -20,17 +20,15 @@ function Bing() {
     },
     success: function (msg) {
       var loop1 = 0;
-      while (loop1 <= 7) {
+      while (loop1 <= 4) {
         image[loop1] = msg['images'][loop1]['urlbase'];
         Date[loop1] = msg['images'][loop1]['enddate'];
         Copy[loop1] = msg['images'][loop1]['copyright'];
         final[loop1] = "http://bing.com" + image[loop1] + "_1366x768.jpg";
-        Dcomp[loop1] = " ("+Month[(Math.floor((Date[loop1]%1000 / 100))-1)]+" "+(Date[loop1]%100)+")";
+        Dcomp[loop1] = " (" + Month[(Math.floor((Date[loop1] % 10000 / 100)) - 1)] + " " + (Date[loop1] % 100) + ")";
         loop1 = loop1 + 1;
-        
 
       }
-      console.log('success');
 
       // build items array
       var items = [
@@ -38,7 +36,13 @@ function Bing() {
           src: final[0],
           w: 1366,
           h: 768,
-          title: Copy[0] + Dcomp[0] 
+          title: Copy[0] + Dcomp[0]
+        },
+        {
+          src: NasaImageResized[0],
+          w: 1366,
+          h: 768,
+          title: NasaImage[0] + Dcomp[0]
         },
         {
           src: final[1],
@@ -47,10 +51,10 @@ function Bing() {
           title: Copy[1] + Dcomp[1]
         },
         {
-          src: final[2],
+          src: NasaImageResized[1],
           w: 1366,
           h: 768,
-          title: Copy[2] + Dcomp[2]
+          title: NasaImage[1] + Dcomp[1]
         }
       ];
 
@@ -64,72 +68,74 @@ function Bing() {
       var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
       gallery.init();
       gallery.listen('afterChange', function () {
+        incimage++;
+        if (incimage <= 4) {
+          gallery.items.push({
+            src: final[incimage],
+            w: 1366,
+            h: 768,
+            title: Copy[incimage] + Dcomp[incimage],
+          },
+            {
+              src: NasaImageResized[incimage],
+              w: 1366,
+              h: 768,
+              title: NasaImage[incimage] + Dcomp[incimage]
+            });
+        }
 
-      var index = gallery.getCurrentIndex();
-      incimage++;
-      if (incimage <= 7) {
-      gallery.items.push({
-        src: final[incimage],
-        w: 1366,
-        h: 768,
-        title: Copy[incimage] + Dcomp[incimage],
-      });
-      } else if(incimage>7 && incimage<=17){
-      gallery.items.push({
-        src: NasaImageResized[incimage-8],
-        w: 1366,
-        h: 768,
-        title: NCopy[incimage-8],
       });
     }
 
   });
-  }
-
-});
 }
 
 $(document).ready(function () {
-  Bing();
-  Nasa();
+  Nasa(Bing);
 });
 
 
 
 var NasaImage = [];
 var inth = 0;
-var inth2 = 0;
+var loopy = 0;
 var NasaImageResized = [];
-
+var len = [];
+var bre = [];
 var NCopy = [];
 
 
-function Nasa() {
-  
-  if (inth != 1) {
-    $.ajax({
-      type: 'GET',
-      dataType: 'json',
-      data: {},
-      url: "http://pipes.yahoo.com/pipes/pipe.run?_id=2FV68p9G3BGVbc7IdLq02Q&_render=json&feedcount=10&feedurl=http%3A%2F%2Fwww.nasa.gov%2Frss%2Fdyn%2Fimage_of_the_day.rss",
-      error: function (jqXHR, textStatus, errorThrown) {
+function Nasa(callback) {
 
-        console.log(jqXHR);
-      },
-      success: function (msg) {
-        inth = 1;
-        var loop = 0;
-        while (loop <= 9) {
-          NasaImage[loop] = msg['value']['items'][loop]['enclosure']['url'];
-          NCopy[loop] = msg['value']['items'][loop]['description'];
-          NasaImageResized[loop] = "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url=" + NasaImage[loop] + "&container=focus&resize_h=768&refresh=2592000";
-          loop = loop + 1;
+  $.get('http://crossorigin.me/http://www.nasa.gov/rss/dyn/image_of_the_day.rss', function (data) {
+    var $xml = $(data);
+    loopy = 0;
+    $xml.find("item").each(function () {
+      var $this = $(this),
+        item = {
+          title: $this.find("title").text(),
+          link: $this.find("link").text(),
+          description: $this.find("description").text(),
+          pubDate: $this.find("pubDate").text(),
+          url: $this.find("enclosure").attr('url'),
+          author: $this.find("author").text()
         }
+      NCopy.push(item);
+      //Do something with item here...
         
 
-
-      }
     });
-  }
-};
+    while (loopy < 5) {
+      NasaImageResized[loopy] = "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url=" + NCopy[loopy].url + "&container=focus&resize_w=1366&resize_h=768&refresh=2592000";
+
+      NasaImage[loopy] = NCopy[loopy].description;
+
+      loopy++;
+    }
+
+    callback();
+  });
+
+
+}
 
